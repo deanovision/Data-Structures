970cde4c7a85d8d92419528ce384531ce4334615
deanovision <rodeanfraser@gmail.com>
2019-07-22 16:14:44 -0400

    setup queue and node

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,42 @@
+class Node:
+    def __init__(self, value=None, next_node=None):
+        self.value = value
+        self.next_node = next_node
+
+    def get_value(self):
+        return self.value
+
+    def get_next(self):
+        return self.next_node
+
+    def set_value(self, value):
+        self.value = value
+
+    def update_next(self, new_next_node):
+        self.next_node = new_next_node
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = []
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        self.storage.append(item)
+        self.increment()
 
-  def len(self):
-    pass
+    def dequeue(self):
+        self.storage.pop(0)
+        self.decrement()
+
+    def len(self):
+        pass
+
+    def increment(self):
+        self.size += 1
+
+    def decrement(self):
+        if self.size >= 0:
+            self.size -= 1
