d0fcd8a5ebce3024ec778cd6f8e13b14d8725ad9
deanovision <rodeanfraser@gmail.com>
2019-07-23 18:11:55 -0400

    updated que and started double linked list

--- queue/queue.py
+++ queue/queue.py
@@ -22,17 +22,54 @@ class Queue:
         # what data structure should we
         # use to store queue elements?
         self.storage = []
+        self.tail = None
+        self.head = None
 
     def enqueue(self, item):
         self.storage.append(item)
         self.increment()
+        # if self.head is None and self.tail is None:
+        #     self.head = Node(item)
+        #     self.tail = Node(item)
+        #     self.increment()
+        #     print(self.size)
+        # elif self.size == 1:
+        #     print(self.head.value)
+        #     self.head.next_node = Node(item)
+        #     print(self.head.next_node.value)
+        #     self.tail = Node(item)
+        #     print(self.tail.value)
+        #     self.increment()
+        # else:
+        #     print(self.size)
+        #     self.tail.next_node = Node(item)
+        #     self.tail = Node(item)
+        #     self.increment()
 
     def dequeue(self):
-        self.storage.pop(0)
-        self.decrement()
+        if self.size > 0:
+            item = self.storage.pop(0)
+            self.decrement()
+            return item
+        # if self.head is None:
+        #     print('queue is currently empty')
+        # elif self.size == 1:
+        #     old_head = self.head
+        #     self.head = None
+        #     self.tail = None
+        #     self.decrement()
+        #     return old_head.value
+        # else:
+        #     print(self.head.value)
+        #     old_head = self.head
+        #     new_head = self.head.next_node
+        #     self.head = new_head
+        #     print(self.head.value)
+        #     self.decrement()
+        #     return old_head.value
 
     def len(self):
-        pass
+        return self.size
 
     def increment(self):
         self.size += 1
