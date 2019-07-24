774b5cfdbc21f9708529bf34b73ed54899dc5a88
deanovision <rodeanfraser@gmail.com>
2019-07-24 18:11:58 -0400

    finished dll and start bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,17 +1,41 @@
 class BinarySearchTree:
-  def __init__(self, value):
-    self.value = value
-    self.left = None
-    self.right = None
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
 
-  def insert(self, value):
-    pass
+    def insert(self, value):
+        # if not self.left and not self.right:
+        #     self.left = BinarySearchTree(value)
+        # if value <= self.left:
+        #   self.left.insert(value)
+        if value <= self.value:
+            if self.left is None:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if self.right is None:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
-  def contains(self, target):
-    pass
+    def contains(self, target):
+        if target == self.value:
+            return True
+        elif target < self.value:
+            if self.left == None:
+                return False
+            else:
+                self.left.contains(target)
+        else:
+            if self.right == None:
+                return False
+            else:
+                self.right.contains(target)
 
-  def get_max(self):
-    pass
+    def get_max(self):
+        pass
 
-  def for_each(self, cb):
-    pass
+    def for_each(self, cb):
+        pass
