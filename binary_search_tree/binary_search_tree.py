cdf644cf885ac3f24d9fc289d6f1b8fa18f613fa
deanovision <rodeanfraser@gmail.com>
2019-07-25 12:45:08 -0400

    finished bst

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -46,9 +46,9 @@ class BinarySearchTree:
     def for_each(self, cb):
         cb(self.value)
         if self.left:
-            return self.left.for_each(cb)
-        elif self.right:
-            return self.right.for_each(cb)
+            self.left.for_each(cb)
+        if self.right:
+            self.right.for_each(cb)
         # else:
         #     cb(self.value)
         #     return
